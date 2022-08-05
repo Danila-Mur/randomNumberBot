af9f26020113e4ce7f6207f89499467a3f600984
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-05 16:17:14 +0300

    add homework for lesson06

---
+++ script.js
@@ -0,0 +1,57 @@
+// const gameQuestion = () => {
+//   const insideFun = () => {
+//     const question = +prompt('Угадай число от 1 до 100');
+//     if (question > 100) {
+//       alert('Загаданное число меньше');
+//       insideFun();
+//     } else if (question < 1) {
+//       alert('Загаданное число больше');
+//       insideFun();
+//     } else if (question !== 0) {
+//       alert('Игра окончена');
+//       // return;
+//     }
+//   };
+//   insideFun();
+// };
+
+// gameQuestion();
+
+const isNumber = (num) => {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+function gameQuestion(x) {
+  function insideFun() {
+    const question = +prompt('Угадай число от 1 до 100');
+
+    console.log(question);
+    console.log(x);
+
+    if (question === null || question === 0) {
+      alert('Игра окончена');
+    } else if (!isNumber(question)) {
+      alert('Введите число!');
+      insideFun();
+    } else if (
+      question !== x &&
+      (question > 100 || question > x) &&
+      question !== 0
+    ) {
+      alert('Загаданное число меньше');
+      insideFun();
+    } else if (
+      question !== x &&
+      (question < 1 || question < x) &&
+      question !== 0
+    ) {
+      alert('Загаданное число больше');
+      insideFun();
+    } else if (question === x && question !== 0) {
+      alert('Поздравляю, Вы угадали!!!');
+    }
+  }
+  insideFun();
+}
+
+gameQuestion(50);
